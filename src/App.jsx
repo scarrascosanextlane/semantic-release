aff4a50c476dae2de4b2c88addccf382f4d9c165
Sergio Carrascosa Sanchez <scarrascosa@nextlane.com>
2026-06-11 12:44:16 +0200

    [CHORE] ACTO-000000: initial React app boilerplate with semantic-release

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Semantic Release Demo</h1>
+      <p>This app uses semantic-release for automated versioning and changelog generation.</p>
+    </div>
+  )
+}
+
+export default App
